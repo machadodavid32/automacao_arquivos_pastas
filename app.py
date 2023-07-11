4fbe19f2b25282731aba2d5559f4ff5fd87f4b6f
David Machado <machadodavid32@gmail.com>
2023-07-10 21:00:36 -0300

    Adicionando novo curso para manipulação, criação e monitoramento de arquivos e pastas.

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+
+print(os.name)
+
+print(os.listdir())
+
+print(os.sep)
+
+caminho = os.path.join(os.getcwd() + os.sep + 'instrucoes.txt')
---
+++ desafios.py
@@ -0,0 +1,16 @@
+import os
+
+# Exiba todos os arquivos da pasta desafio_arquivos
+
+
+
+# Monte e exiba o caminho(path) dos 3 arquivos desta pasta
+
+# Monte e exiba o caminho(path) que estão dentro das subpasta(desafios_texto)
+
+# Navegue para as 3 seguintes pastas, usando o os.chdir():
+    # pasta desafios_texto
+    # de vola para a pasta desafios_arquivos
+    # Diretório pai da pasta desafio_arquivos
+    
+    
