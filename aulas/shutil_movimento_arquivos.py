36e2270c117416da8e230d0601781663feec401c
David Machado <machadodavid32@gmail.com>
2023-08-21 21:16:09 -0300

    foi criado uma pasta chamado musica dentro da pasta chamada fotos e foi repassado os arquivos da pasta musica original para dentro desta pasta musica localizada dentro da pasta fotos

--- aulas/shutil_movimento_arquivos.py
+++ aulas/shutil_movimento_arquivos.py
@@ -8,8 +8,13 @@ relatorio = os.getcwd() + os.sep + 'relatorio.pdf'
 fotos = os.getcwd() + os.sep + 'fotos'
 shutil.copy(relatorio, fotos)
 """
-# Copiar uma pasta toda
+# Copiar conteúdo de uma pasta para outra
 caminho_pasta_musica = os.getcwd() + os.sep + 'musica'
 fotos = os.getcwd() + os.sep + 'fotos'
-shutil.copytree(caminho_pasta_musica, fotos, dirs_exist_ok=True)
+#shutil.copytree(caminho_pasta_musica, fotos, dirs_exist_ok=True)
+
+# copiar pasta e seus conteúdos para uma nova pasta.
+os.makedirs('fotos' + os.sep + 'musica') # Criando a pasta musica dentro da pasta fotos
+pasta_musica_em_pasta_fotos = os.getcwd() + os.sep + 'fotos' + os.sep + 'musica' # criando um caminho p/ pasta musica dentro da pasta fotos
+shutil.copytree(caminho_pasta_musica, pasta_musica_em_pasta_fotos, dirs_exist_ok=True)
 
